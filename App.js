4f8eff7f36e69c792f9adfa70c3f12c4038ae440
MarvinArtiga <marvin.artigagonzalez@gmail.com>
2024-04-12 12:53:09 -0600

    new branch

--- App.js
+++ App.js
@@ -1,22 +1,70 @@
-import React from 'react';
-import { NavigationContainer } from '@react-navigation/native';
-import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
-import SolarSystemScreen from './screens/SolarSystemScreen';
-import EarthScreen from './screens/EarthScreen';
-import SunScreen from './screens/SunScreen';
+import react from "react";
+import Navigation from "./Navigation";
 
-const Tab = createBottomTabNavigator();
-
-const App = () => {
+export default function App(){
   return (
-    <NavigationContainer>
-      <Tab.Navigator>
-        <Tab.Screen name="Sistema Solar" component={SolarSystemScreen} />
-        <Tab.Screen name="Tierra" component={EarthScreen} />
-        <Tab.Screen name="Sol" component={SunScreen} />
-      </Tab.Navigator>
-    </NavigationContainer>
-  );
-};
+    <View style={styles.container}>
+      <Text style={styles.titulo}>Bienvenido!</Text>
+      <Text style={styles.subtitulo}>Inicia  sesion con tu cuenta.</Text>
+      <TextInput 
+        placeholder='Usuario'
+        style={styles.input}
+      />
 
-export default App;
+      <TextInput 
+        placeholder='Contraseña'
+        style={styles.input}
+      />
+      <TouchableOpacity
+        style={styles.button}
+        onPress={() => {
+        }}
+      >
+        <Text style={styles.buttonText}>Iniciar Sesión</Text>
+      </TouchableOpacity>
+
+      <Text></Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  titulo: {
+    fontSize: 60,
+    color: '#567FFF',
+    fontWeight: 'bold',
+  },
+  subtitulo: {
+    fontSize: 20,
+    color: 'gray',
+  },
+  input: {
+    borderWidth: 1,
+    paddingStart: 30,
+    padding: 10,
+    width: '80%',
+    height: 50,
+    marginTop: 20,
+    borderRadius: 30,
+    backgroundColor: '#f1f1f1',
+  },
+  button: {
+    backgroundColor: '#567FFF',
+    padding: 15,
+    marginTop: 20,
+    borderRadius: 30,
+    width:'45%',
+  },
+  buttonText: {
+    color: '#fff',
+    textAlign: 'center',
+    fontSize: 18,
+    fontWeight: 'bold',
+  },
+});
